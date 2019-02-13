6f188a3062e3cefe640dfe929b1050448026f9e2
Roman <roman.ivan@mail.ru>
2019-02-14 01:42:40 +0300

    Change Stantion, Train. Added Hash#type_trains

--- lesson3/station_class.rb
+++ lesson3/station_class.rb
@@ -1,141 +1,108 @@
-class Stantion()
-  attr_accessor :trains
-  attr_reader :name
+class Stantion
+  attr_reader :trains
+  attr_accessor :name, :type_trains
 
   def initialize(name)
     @name = name
     @trains = []
+    @type_trains = { passanger: 0, cargo: 0 }
   end
 
   def add_train(train)
-    self.trains << train
-    train.train_stantion = self
-  end
-
-  def show_trains_on_station
-    self.trains.each {|train| puts train.object_id.to_s}
+    @trains << train
+    @type_trains[train.type_train] += 1
   end
 
   def delete_train(train)
-    self.trains.delete(train)
-    self.trains.each {|t| t.delete_stantion(trains) if t == train}
+    @trains.delete(train)
+    @type_trains[train.type_train] -= 1
   end
 
-  def amount_trains(type_index)
-    count = 0
-    type_train = Train::TYPE[type_index]
-    self.trains.each do |train|
-      if train.type_train == type_train
-        count += 1
-        puts train
-      end
-    end
-    puts "Amount of trains by type #{type_train} = " + count.to_s
+  def all_train
+    @type_trains[:passanger] + @type_trains[:cargo]
   end
-
 end
 
 class Route
   attr_reader :stantions
 
-  def initialize(first,last)
-    @stantions = [first,last]
+  def initialize(first_stantion, last_stantion)
+    @stantions = [first_stantion, last_stantion]
   end
 
-  #Добавляем станции последовательно от станции отправки
-  def add_stantion(name)
-    @stantions.insert(-2, name) unless @stantions.include?(name)
+  def add_stantion(stantion)
+    @stantions.insert(-2, stantion) unless @stantions.include?(stantion)
   end
 
   def delete_stantion(name)
-    @stantions.delete(name) unless ((@stantions.first == name) || (@stantions.last == name))
-  end
-
-  def show_all_route
-     @stantions.each {|station| puts station.name}
-     puts "count= " + @stantions.size.to_s
+    @stantions.delete(name) unless (@stantions.first == name) || (@stantions.last == name)
   end
 end
 
 class Train
-  #attr_writer :train_stantion
-  attr_reader :number_train, :type_train, :route_train
-  attr_accessor :speed, :wagons, :train_stantion
-  TYPE = [:passanger, :cargo]
-  def initialize(number_train, index_type, wagons = 1)
+  attr_writer :index_station
+  attr_reader :number_train, :type_train, :speed, :wagons, :stantion, :index_station
+  # type_train = [:passanger, :cargo]
+  def initialize(number_train, type_train, wagons = 1)
     @number_train = number_train
-    @type_train = TYPE[index_type]
+    @type_train = type_train
     @wagons = wagons
     @speed = 0
   end
 
   def add_speed(count = 10)
-    self.speed += count
-  end
-
-  def current_speed
-  puts self.speed
+    @speed += count if (@speed + count) >= 0
   end
 
   def stop
-    self.speed = 0
+    @speed = 0
   end
 
   def add_wagon
-    self.wagons +=1 if self.speed.zero?
+    @wagons += 1 if @speed.zero?
   end
 
   def delete_wagon
-    self.wagons -=1 if (self.speed.zero? && self.wagons > 0)
+    @wagons -= 1 if @speed.zero? && @wagons > 0
   end
 
-  def set_route_train(route)
+  def route_train(route)
     @route_train = route
-    @train_stantion = route.stantions.first
-    self.train_stantion.add_train(self)
-    puts "train on stantion " + self.train_stantion.name
+    @stantion = route.stantions.first
+    @stantion.add_train(self)
+    @index_station = 0
   end
 
   def delete_station
-     self.train_stantion = nil unless self.train_stantion.nil?
+    @stantion = nil
+    @index_station = nil
   end
 
-
-  def to_last_step_station
-    self.train_stantion.delete_train(self)
-    self.train_stantion = self.route_train.stantions[index_station+1] if index_station < route_train.stantions.size
-    self.train_stantion.add_train(self)
-    puts self.train_stantion.name
-  end
-
-  def to_first_step_station
-  self.train_stantion.delete_train(self)
-  self.train_stantion = self.route_train.stantions[index_station-1] if index_station >= 0
-  self.train_stantion.add_train(self)
-  end
-
-  def index_station
-    i = self.route_train.stantions.index(self.train_stantion)
-    return i if (i >= 0) && (i <= self.route_train.stantions.size)
-  end
-
-  def info_station
-    puts "Текущая станция " +  self.train_stantion.name
-    puts "Хотите узнать следующую станцию, введите 'next'"
-    puts "Хотите узнать предыдущую станцию, введите 'previous'"
-    what_station = gets.chomp
-
-    case what_station
-    when 'next'
-      puts "Next station name " + self.route_train.stantions[index_station+1].name
-    when 'previous'
-      puts "Next station name " + self.route_train.stantions[index_station-1].name
-    else
-      puts "No correct date"
+  def got_to_next_station
+    next_st = next_station
+    unless next_st.nil?
+      @stantion.delete_train(self)
+      @stantion = next_st
+      @index_station += 1
+      @stantion.add_train(self)
     end
-
   end
 
+  def got_to_previous_station
+    previous_st = previous_station
+    unless previous_st.nil?
+      @stantion.delete_train(self)
+      @stantion = previous_station
+      @index_station -= 1
+      @stantion.add_train(self)
+    end
+  end
+
+  def next_station
+    return @route_train.stantions[@index_station + 1] if @index_station < @route_train.stantions.size - 1
+  end
+
+  def previous_station
+    return @route_train.stantions[@index_station - 1] if @index_station > 0
+  end
 end
-
-
